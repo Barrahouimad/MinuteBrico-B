279e64651213028fd1c784f32ac7eca6500b1ad7
iliass alilou <iliassalilou18@gmail.com>
2021-06-08 10:57:52 +0100

    email exist

--- src/main/java/ma/ac/emi/MinuteBrico/Repositories/ClientRepository.java
+++ src/main/java/ma/ac/emi/MinuteBrico/Repositories/ClientRepository.java
@@ -1,17 +1,19 @@
 package ma.ac.emi.MinuteBrico.Repositories;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import ma.ac.emi.MinuteBrico.Models.BricoleurModel;
+
 import  ma.ac.emi.MinuteBrico.Models.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
 	 List<Client> findByEmail(String email);
 	 Client findById(int id);
-	List<Client> findByEmailAndPassword(String email, String password);
-	Client findByToken(String token);
+	 List<Client> findByEmailAndPassword(String email, String password);
+	 Client findByToken(String token);
+	 
+	 //Optional<Client> findByEmail1(String email);
 
 }
